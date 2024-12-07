b3779db27c1738dff2d42f4d1edadefff876e201
Losev127 <145589318+Losev127@users.noreply.github.com>
2024-12-07 19:18:15 +0300

    Вывод информации на главную страницу

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from Dynasty_8.views import index_page, create_adv, create_app
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,3 +26,6 @@ urlpatterns = [
     path('create/', create_adv, name='create_adv'),
     path('new_app/', create_app, name='create_app'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
