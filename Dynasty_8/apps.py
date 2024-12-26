d54111e6eabf0877cb019f176c40dbb9fdc6000d
Losev127 <145589318+Losev127@users.noreply.github.com>
2024-12-26 15:38:07 +0300

    Добавление сигналов в приложение

--- Dynasty_8/apps.py
+++ Dynasty_8/apps.py
@@ -4,3 +4,11 @@ from django.apps import AppConfig
 class Dynasty8Config(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'Dynasty_8'
+
+class Dynasty8Config(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Dynasty_8'
+
+# Dynasty_8/apps.py
+def ready(self):
+    import Dynasty_8.signals
